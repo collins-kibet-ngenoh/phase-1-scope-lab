56a8067252d1a8ec5d557519326623b4bf05745c
collins-kibet-ngenoh <collins.kibet.ngenoh@gmail.com>
2024-03-22 16:53:55 +0300

    scope lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your solution in this file!
+var customerName = "bob";
+console.log(customerName);
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase()
+    return customerName;
+}
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+    return bestCustomer;
+}
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob'
+}
+const leastFavoriteCustomer = 'John Doe';
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'Jane Smith'
+}
